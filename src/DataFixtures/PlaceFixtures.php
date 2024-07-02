e901a3d24eb806835a46844b911d911b4aec9ef7
gR9z <royergaetan90@gmail.com>
2024-07-02 17:01:19 +0200

    add js for City and Places

--- src/DataFixtures/PlaceFixtures.php
+++ src/DataFixtures/PlaceFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Place;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -10,10 +11,32 @@ class PlaceFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = \Faker\Factory::create('en_US');
+
+        $cityReferences = [];
+        for ($i = 0; $i < 20; $i++) {
+            $cityReferences[] = $this->getReference('city_' . $i);
+        }
+
+        $places = [];
+
+        for ($i = 0; $i < 100; $i++) {
+            $place = new Place();
+            $place->setName($faker->company);
+            $place->setStreet($faker->streetAddress);
+            $place->setLongitude($faker->longitude);
+            $place->setLatitude($faker->latitude);
+            $place->setCity($faker->randomElement($cityReferences));
+
+            $manager->persist($place);
+            $places[] = $place;
+        }
 
         $manager->flush();
+
+        foreach ($places as $i => $place) {
+            $this->addReference('place_' . $i, $place);
+        }
     }
 
     public function getDependencies(): array
