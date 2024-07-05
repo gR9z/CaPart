acf40d0934fa8d0ce3240769c3b35c3e01b05341
gR9z <royergaetan90@gmail.com>
2024-07-05 16:31:29 +0200

    registration ok

--- src/Service/EventService.php
+++ src/Service/EventService.php
@@ -6,18 +6,18 @@ use App\Entity\Event;
 use App\Entity\State;
 use App\Entity\User;
 use App\Enum\StateLabel;
-use App\Form\EventType;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Form\FormFactoryInterface;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class EventService
 {
     private EntityManagerInterface $entityManager;
+    private UserService $userService;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, UserService $userService, SluggerInterface $slugger)
     {
         $this->entityManager = $entityManager;
+        $this->userService = $userService;
     }
 
     public function createEvent(User $user, Event $event): array
@@ -46,5 +46,90 @@ class EventService
             'event' => $event,
             'message' => 'Event created!'
         ];
+    }
+
+    public function registerUserToEvent(Event $event): array
+    {
+        $user = $this->userService->getAuthenticatedUser();
+
+        $validationResult = $this->validateRegistration($event, $user);
+        if (!$validationResult['success']) {
+            return $validationResult;
+        }
+
+        $event->addParticipant($user);
+        $this->entityManager->persist($event);
+        $this->entityManager->flush();
+
+        return [
+            'success' => true,
+            'message' => 'You have successfully registered for the event'
+        ];
+    }
+
+    private function validateRegistration(Event $event, User $user): array
+    {
+        $checks = [
+            $this->checkIfUserIsRegistered($event, $user),
+            $this->checkRegistrationDeadline($event),
+            $this->checkIfEventIsFull($event),
+            $this->checkIfEventIsOpen($event)
+        ];
+
+        foreach ($checks as $check) {
+            if (!$check['success']) {
+                return $check;
+            }
+        }
+
+        return ['success' => true];
+    }
+
+    private function checkIfUserIsRegistered(Event $event, User $user): array
+    {
+        if ($event->isUserRegistered($user)) {
+            return [
+                'success' => false,
+                'message' => 'You are already registered for this event'
+            ];
+        }
+
+        return ['success' => true];
+    }
+
+    private function checkRegistrationDeadline(Event $event): array
+    {
+        if (new \DateTime() > $event->getRegistrationDeadline()) {
+            return [
+                'success' => false,
+                'message' => 'The registration deadline has passed'
+            ];
+        }
+
+        return ['success' => true];
+    }
+
+    private function checkIfEventIsOpen(Event $event): array
+    {
+        if ($event->getState()->getLabel() !== 'open') {
+            return [
+                'success' => false,
+                'message' => 'The event is not open for registration'
+            ];
+        }
+
+        return ['success' => true];
+    }
+
+    private function checkIfEventIsFull(Event $event): array
+    {
+        if ($event->getParticipants()->count() >= $event->getMaxRegistrations()) {
+            return [
+                'success' => false,
+                'message' => 'The event is full'
+            ];
+        }
+
+        return ['success' => true];
     }
 }
