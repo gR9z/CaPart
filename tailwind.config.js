277dc9ceb4d7299dc83732454b8ad4ea5d227f42
gR9z <royergaetan90@gmail.com>
2024-07-09 13:53:19 +0200

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: [
     "./assets/**/*.js",
     "./templates/**/*.html.twig",
-    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
