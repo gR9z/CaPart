b2fa4dc12b77035f4f3762d9b10deef06a305c8a
gR9z <royergaetan90@gmail.com>
2024-07-11 09:59:44 +0200

    fix csss

--- src/Controller/CityController.php
+++ src/Controller/CityController.php
@@ -10,8 +10,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Routing\Attribute\Route;
 
 class CityController extends AbstractController
 {
